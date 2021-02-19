23a9ab4f3ba22debc90096878c0d36a0569419d7
Ocrosoft <ocrosoft@ocrosoft.com>
2021-02-19 23:16:33 +0800

    [improve] 支持显示tag

--- pixiv previewer.user.js
+++ pixiv previewer.user.js
@@ -2786,6 +2786,10 @@ function PixivNS(callback) {
         return ul;
     }
 
+    function getSearchParamsWithoutPage() {
+        return location.search.substr(1).replace(/&?p=\d+/);
+    }
+
     function getNovelTemplate(ul) {
         if (!ul) {
             return null;
@@ -2810,7 +2814,7 @@ function PixivNS(callback) {
         let bookmarkDiv = tagDiv.children().eq(0);
         bookmarkDiv.children().eq(0).addClass('pns-text-count');
         if (bookmarkDiv.children().length < 2) {
-            bookmarkDiv.find('.pns-text-count').after('<div class="sc-1c4k3wn-10 huRoWZ pns-bookmark-div"><span class="sc-14heosd-0 hZsYvc"><svg viewBox="0 0 12 12" size="12" class="sc-14heosd-1 dcwYur"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 0.75C10.6569 0.75 12 2.09315 12 3.75C12 7.71703 7.33709 10.7126   6.23256 11.3666C6.08717 11.4526 5.91283 11.4526 5.76744 11.3666C4.6629 10.7126 0 7.71703 0 3.75C0 2.09315 1.34315 0.75 3 0.75C4.1265 0.75 5.33911 1.60202 6 2.66823C6.66089 1.60202 7.8735 0.75 9 0.75Z"></path></svg></span><span class="sc-1c4k3wn-11 jlMYck pns-bookmark-count">0</span></div>')
+            bookmarkDiv.find('.pns-text-count').after('<div class="pns-bookmark-div"><span style="display: inline-flex; vertical-align: top; height: 20px;"><svg viewBox="0 0 12 12" size="12" class="sc-14heosd-1 dcwYur"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 0.75C10.6569 0.75 12 2.09315 12 3.75C12 7.71703 7.33709 10.7126   6.23256 11.3666C6.08717 11.4526 5.91283 11.4526 5.76744 11.3666C4.6629 10.7126 0 7.71703 0 3.75C0 2.09315 1.34315 0.75 3 0.75C4.1265 0.75 5.33911 1.60202 6 2.66823C6.66089 1.60202 7.8735 0.75 9 0.75Z"></path></svg></span><span class="pns-bookmark-count" style="padding-left: 4px;">0</span></div>')
         } else {
             bookmarkDiv.find('span:last').addClass('pns-bookmark-count').parent().addClass('pns-bookmark-div');
         }
@@ -2847,15 +2851,25 @@ function PixivNS(callback) {
             template.find('.pns-bookmark-count').text(novel.bookmarkCount);
         }
         let tagList = template.find('.pns-tag-list');
-        tagList.hide(); // 暂不支持
-        template.find('.pns-desc').text(novel.description).attr('title', novel.description);
+        let search = getSearchParamsWithoutPage();
+        if (search.length > 0) {
+            search = '?' + search;
+        }
+        $.each(novel.tags, function (i, tag) {
+            let tagItem = $('<span"><a style="color: rgb(61, 118, 153);" href="/tags/' + tag + '/novels' + search + '">' + tag + '</a></span>');
+            if (tag == 'R-18' || tag == 'R-18G') {
+                tagItem.find('a').css({'color': 'rgb(255, 64, 96)', 'font-weight': 'bold'}).text(tag);
+            }
+            tagList.append(tagItem);
+        });
+        template.find('.pns-desc').html(novel.description).attr('title', template.find('.pns-desc').text());
         template.find('.pns-like').hide(); // 暂不支持
         return template;
     }
 
     function getNovelByPage(key, from, to) {
         let url = location.origin + g_getNovelUrl.replace(/#key#/g, key).replace(/#page#/g, from);
-        let search = location.search.substr(1).replace(/&?p=\d+/);
+        let search = getSearchParamsWithoutPage();
         if (search.length > 0) {
             url += '&' + search;
         }
