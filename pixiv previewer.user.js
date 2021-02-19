6494f5aaa848c9767110013dfaa3d58d6f3b68a8
Ocrosoft <ocrosoft@ocrosoft.com>
2021-02-19 18:14:58 +0800

    [improve] jquery官方cdn访问不了的次数有点多，换成百度的试试。

--- pixiv previewer.user.js
+++ pixiv previewer.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name                Pixiv Previewer
 // @namespace           https://github.com/Ocrosoft/PixivPreviewer
-// @version             3.4.1
+// @version             3.4.2
 // @description         Display preview images (support single image, multiple images, moving images); Download animation(.zip); Sorting the search page by favorite count(and display it). Updated for the latest search page.
 // @description:zh-CN   显示预览图（支持单图，多图，动图）；动图压缩包下载；搜索页按热门度（收藏数）排序并显示收藏数，适配11月更新。
 // @description:ja      プレビュー画像の表示（単一画像、複数画像、動画のサポート）; アニメーションのダウンロード（.zip）; お気に入りの数で検索ページをソートします（そして表示します）。 最新の検索ページ用に更新されました。
@@ -18,7 +18,7 @@ try {
     $();
 } catch (e) {
     let script = document.createElement('script');
-    script.src = 'https://code.jquery.com/jquery-2.2.4.min.js';
+    script.src = 'http://libs.baidu.com/jquery/2.1.4/jquery.min.js';
     document.head.appendChild(script);
 }
 
