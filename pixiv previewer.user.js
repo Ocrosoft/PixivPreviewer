6e1876326ad71bef7f080abc97d8041328efe4b5
Ocrosoft <ocrosoft@ocrosoft.com>
2024-02-20 21:43:23 +0800

    fix: 修复 loading 偏移的问题

--- pixiv previewer.user.js
+++ pixiv previewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                Pixiv Previewer(Dev)
 // @namespace           https://github.com/Ocrosoft/PixivPreviewer
-// @version             3.7.19
+// @version             3.7.20
 // @description         Display preview images (support single image, multiple images, moving images); Download animation(.zip); Sorting the search page by favorite count(and display it). Updated for the latest search page.
 // @description:zh-CN   显示预览图（支持单图，多图，动图）；动图压缩包下载；搜索页按热门度（收藏数）排序并显示收藏数，适配11月更新。
 // @description:ja      プレビュー画像の表示（単一画像、複数画像、動画のサポート）; アニメーションのダウンロード（.zip）; お気に入りの数で検索ページをソートします（そして表示します）。 最新の検索ページ用に更新されました。
@@ -2200,7 +2200,7 @@ function PixivPreview() {
 
             // 加载中图片
             let loadingImg = $(new Image()).addClass('pp-loading').attr('src', g_loadingImage).css({
-                'position': 'absolute', 'border-radius': '20px',
+                'position': 'absolute', 'border-radius': '20px', 'left': '0px', 'top': '0px'
             });
             previewDiv.append(loadingImg);
 
