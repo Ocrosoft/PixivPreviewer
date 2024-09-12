6d288bd09de90c51de84b33c49c5745299c6a269
Ocrosoft <ocrosoft@ocrosoft.com>
2024-09-13 00:23:16 +0800

    fix: 修复 root div 的 id 在部分页面下调整为 __next，导致找不到工具栏的问题

--- pixiv previewer.user.js
+++ pixiv previewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                Pixiv Previewer(Dev)
 // @namespace           https://github.com/Ocrosoft/PixivPreviewer
-// @version             3.7.23
+// @version             3.7.24
 // @description         Display preview images (support single image, multiple images, moving images); Download animation(.zip); Sorting the search page by favorite count(and display it). Updated for the latest search page.
 // @description:zh-CN   显示预览图（支持单图，多图，动图）；动图压缩包下载；搜索页按热门度（收藏数）排序并显示收藏数，适配11月更新。
 // @description:ja      プレビュー画像の表示（単一画像、複数画像、動画のサポート）; アニメーションのダウンロード（.zip）; お気に入りの数で検索ページをソートします（そして表示します）。 最新の検索ページ用に更新されました。
@@ -1036,7 +1036,10 @@ let ControlElementsAttributesSample = {
 };
 
 function findToolbarCommon() {
-    return $('#root').find('ul:last').get(0);
+    let rootToolbar = $('#root').find('ul:last').get(0);
+    if (rootToolbar) return rootToolbar;
+    let nextToolbar = $('#__next').find('ul:last').get(0);
+    return nextToolbar;
 }
 function findToolbarOld() {
     return $('._toolmenu').get(0);
