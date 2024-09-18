947e4aa9939e72181e236e36cd0ca17139627ac1
Ocrosoft <ocrosoft@ocrosoft.com>
2024-09-19 00:05:13 +0800

    fix: 修复动图详情页不显示下载按钮的问题

--- pixiv previewer.user.js
+++ pixiv previewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                Pixiv Previewer(Dev)
 // @namespace           https://github.com/Ocrosoft/PixivPreviewer
-// @version             3.7.25
+// @version             3.7.26
 // @description         Display preview images (support single image, multiple images, moving images); Download animation(.zip); Sorting the search page by favorite count(and display it). Updated for the latest search page.
 // @description:zh-CN   显示预览图（支持单图，多图，动图）；动图压缩包下载；搜索页按热门度（收藏数）排序并显示收藏数，适配11月更新。
 // @description:ja      プレビュー画像の表示（単一画像、複数画像、動画のサポート）; アニメーションのダウンロード（.zip）; お気に入りの数で検索ページをソートします（そして表示します）。 最新の検索ページ用に更新されました。
@@ -1751,7 +1751,13 @@ Pages[PageType.Artwork] = {
             /^https:\/\/www.pixiv.net\/en\/artworks\/.*/.test(url)
     },
     ProcessPageElements: function () {
-       let returnMap = {
+        let canvas = $('main').find('figure').find('canvas');
+        if ($('main').find('figure').find('canvas').length > 0) {
+            this.private.needProcess = true;
+            canvas.addClass('pp-canvas');
+        }
+
+        let returnMap = {
             loadingComplete: false,
             controlElements: [],
         };
