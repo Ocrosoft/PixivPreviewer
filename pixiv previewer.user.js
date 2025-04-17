cb40ee77354d21c7614dfe17ac0c53eb92e7de9d
Ocrosoft <ocrosoft@ocrosoft.com>
2025-04-17 17:07:52 +0800

    fix: 修复添加收藏失败的问题

--- pixiv previewer.user.js
+++ pixiv previewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                Pixiv Previewer(Dev)
 // @namespace           https://github.com/Ocrosoft/PixivPreviewer
-// @version             3.7.33
+// @version             3.7.34
 // @description         Display preview images (support single image, multiple images, moving images); Download animation(.zip); Sorting the search page by favorite count(and display it). Updated for the latest search page.
 // @description:zh-CN   显示预览图（支持单图，多图，动图）；动图压缩包下载；搜索页按热门度（收藏数）排序并显示收藏数，适配11月更新。
 // @description:ja      プレビュー画像の表示（単一画像、複数画像、動画のサポート）; アニメーションのダウンロード（.zip）; お気に入りの数で検索ページをソートします（そして表示します）。 最新の検索ページ用に更新されました。
@@ -4465,7 +4465,7 @@ function Load() {
     // g_csrfToken
     if (g_pageType == PageType.Search || g_pageType == PageType.NovelSearch) {
         $.get(location.href, function (data) {
-            let matched = data.match(/token":"([a-z0-9]{32})/);
+            let matched = data.match(/token\\":\\"([a-z0-9]{32})/);
             if (matched.length > 0) {
                 g_csrfToken = matched[1];
                 DoLog(LogLevel.Info, 'Got g_csrfToken: ' + g_csrfToken);
