e50f6b85411b403fd9aec5382be960b02cc7e9a2
Ocrosoft <ocrosoft@ocrosoft.com>
2025-05-20 16:02:20 +0800

    chore: update version to 3.7.39, update description meta data

--- pixiv previewer.user.js
+++ pixiv previewer.user.js
@@ -1,11 +1,16 @@
 // ==UserScript==
-// @name                Pixiv Previewer(Dev)
+// @name                Pixiv Previewer (Dev)
+// @name:ja             Pixiv Previewer (Dev)
+// @name:ru             Pixiv Previewer (Dev)
+// @name:zh-CN          Pixiv Previewer (Dev)
+// @name:zh-TW          Pixiv Previewer (Dev)
 // @namespace           https://github.com/Ocrosoft/PixivPreviewer
-// @version             3.7.38
+// @version             3.7.39
 // @description         Display preview images (support single image, multiple images, moving images); Download animation(.zip); Sorting the search page by favorite count(and display it). Updated for the latest search page.
 // @description:zh-CN   显示预览图（支持单图，多图，动图）；动图压缩包下载；搜索页按热门度（收藏数）排序并显示收藏数，适配11月更新。
 // @description:ja      プレビュー画像の表示（単一画像、複数画像、動画のサポート）; アニメーションのダウンロード（.zip）; お気に入りの数で検索ページをソートします（そして表示します）。 最新の検索ページ用に更新されました。
-// @description:zh_TW   顯示預覽圖像（支持單幅圖像，多幅圖像，運動圖像）； 下載動畫（.zip）; 按收藏夾數對搜索頁進行排序（並顯示）。 已為最新的搜索頁面適配。
+// @description:zh-TW   顯示預覽圖像（支持單幅圖像，多幅圖像，運動圖像）； 下載動畫（.zip）; 按收藏夾數對搜索頁進行排序（並顯示）。 已為最新的搜索頁面適配。
+// @description:ru      Отображение превью изображений (поддержка одиночных, множественных и анимированных изображений); Скачивание анимаций (.zip); Сортировка страницы поиска по количеству добавлений в закладки (с отображением количества). Обновлено для последней версии поисковой страницы.
 // @author              Ocrosoft
 // @match               *://www.pixiv.net/*
 // @grant               unsafeWindow
