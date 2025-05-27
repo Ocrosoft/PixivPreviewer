4027d5f6002e69795ae599ff5a73ef697ebb5c7d
ocrosoft <ocrosoft@ocrosoft.com>
2025-05-27 20:04:45 +0800

    chore: 更新版本号 3.7.41

--- pixiv previewer.user.js
+++ pixiv previewer.user.js
@@ -5,7 +5,7 @@
 // @name:zh-CN          Pixiv Previewer (Dev)
 // @name:zh-TW          Pixiv Previewer (Dev)
 // @namespace           https://github.com/Ocrosoft/PixivPreviewer
-// @version             3.7.40
+// @version             3.7.41
 // @description         Display preview images (support single image, multiple images, moving images); Download animation(.zip); Sorting the search page by favorite count(and display it). Updated for the latest search page.
 // @description:zh-CN   显示预览图（支持单图，多图，动图）；动图压缩包下载；搜索页按热门度（收藏数）排序并显示收藏数，适配11月更新。
 // @description:ja      プレビュー画像の表示（単一画像、複数画像、動画のサポート）; アニメーションのダウンロード（.zip）; お気に入りの数で検索ページをソートします（そして表示します）。 最新の検索ページ用に更新されました。
